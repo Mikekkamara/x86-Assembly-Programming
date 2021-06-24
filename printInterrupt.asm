6ef9a8143bf08c5593d5ffdb3ded2dc01d1eab7e
Mikekkamara <mikekkamara@gmail.com>
2021-06-24 11:34:54 +0300

    new

---
+++ printInterrupt.asm
@@ -0,0 +1,28 @@
+
+org 100h
+
+MOV AH, 0Eh
+
+MOV AL, 'H'
+INT 10h
+
+MOV AL, 'e'
+INT 10h
+
+MOV AL, 'l'
+INT 10h
+
+MOV AL, 'l'
+INT 10h
+
+MOV AL, 'o'
+INT 10h
+
+MOV AL, '!'
+INT 10h
+
+ret
+
+
+
+
